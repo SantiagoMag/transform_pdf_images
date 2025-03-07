43d0b37ceb845990a5445ca1ae4849075e8078e8
developer77oa <developer01@systemouts.com>
2025-03-07 12:49:20 -0500

    UPD lambda_handler for event

--- app.py
+++ app.py
@@ -14,63 +14,69 @@ DESTINATION_FOLDER = "invoking_bedrock_classification/proccesed/"
 
 
 def lambda_handler(event, context):
-    # The path to the 'pdftoppm' tool
-    #batch_id = event['batch_id']
-
-    try:
-        batch_id = json.loads(event['body']).get('batch_id')  # Si viene en el body (POST)
-
-        filter_expression = "#s = :open"
-        expression_names = {"#s": "status"}
-        expression_values = {":open": {"S": "open"}}
-
-        if batch_id:
-            filter_expression += " AND #b = :batch_id"
-            expression_names["#b"] = "batch_id"
-            expression_values[":batch_id"] = {"S": batch_id}
+    # EVENTO DE SQS
+    print("Event received:", json.dumps(event))
         
-        response = dynamodb.scan(
-            TableName=TABLE_NAME,
-            FilterExpression=filter_expression,
-            ExpressionAttributeNames=expression_names,
-            ExpressionAttributeValues=expression_values
-        )
+    try:
+        for record in event['Records']:
+            batch_id = json.loads(record['body']).get('batch_id')
+            #batch_id = json.loads(event['body']).get('batch_id')
+
+            filter_expression = "#s = :open"
+            expression_names = {"#s": "status"}
+            expression_values = {":open": {"S": "open"}}
+
+            if batch_id:
+                filter_expression += " AND #b = :batch_id"
+                expression_names["#b"] = "batch_id"
+                expression_values[":batch_id"] = {"S": batch_id}
+            
+            response = dynamodb.scan(
+                TableName=TABLE_NAME,
+                FilterExpression=filter_expression,
+                ExpressionAttributeNames=expression_names,
+                ExpressionAttributeValues=expression_values
+            )
 
 
-        if 'Items' in response:
+            if 'Items' in response:
 
-            docs_proccesed = []
-            for item in response['Items']:
-                object_key =  item['obj_key']['S']
-                case_id = item['case_id']['S']
+                docs_proccesed = []
+                for item in response['Items']:
+                    object_key =  item['obj_key']['S']
+                    case_id = item['case_id']['S']
 
-                if object_key.endswith(".pdf"):
-                    try:
-                        # Descargar el PDF desde S3
-                        pdf_temp_path = download_pdf_from_s3(BUCKET_NAME, object_key)
-                        
-                        # Convertir PDF a imágenes
-                        images = pdf_to_images(pdf_temp_path)
-                        
-                        # Subir imágenes a S3
-                        upload_images_to_s3(BUCKET_NAME, case_id, object_key, images)
-                        
-                        print(f"PDF {object_key} procesado y guardado en {DESTINATION_FOLDER}")
-                        docs_proccesed.append(object_key)
-                    except Exception as e:
-                        print(f"Error procesando el PDF: {str(e)}")
-                else:
-                    print(f"Archivo ignorado: {object_key}")
+                    if object_key.endswith(".pdf"):
+                        try:
+                            update_dynamodb_status(item['case_id']['S'],"processing_capture")
+                            
+                            # Descargar el PDF desde S3
+                            pdf_temp_path = download_pdf_from_s3(BUCKET_NAME, object_key)
+                            
+                            # Convertir PDF a imágenes
+                            images = pdf_to_images(pdf_temp_path)
+                            
+                            # Subir imágenes a S3
+                            upload_images_to_s3(BUCKET_NAME, case_id, object_key, images)
+                                
+                            update_dynamodb_status(item['case_id']['S'],"processed_capture")
+                            
+                            print(f"PDF {object_key} procesado y guardado en {DESTINATION_FOLDER}")
+                            docs_proccesed.append(object_key)
+                        except Exception as e:
+                            print(f"Error procesando el PDF: {str(e)}")
+                    else:
+                        print(f"Archivo ignorado: {object_key}")
 
-            return {
-                'statusCode': 200,
-                'body': json.dumps(docs_proccesed)  # Return the items that meet the filter criteria
-            }
-        else:
-            return {
-                'statusCode': 404,
-                'body': json.dumps({"message": "No items found matching criteria"})
-            }
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps(docs_proccesed)  # Return the items that meet the filter criteria
+                }
+            else:
+                return {
+                    'statusCode': 404,
+                    'body': json.dumps({"message": "No items found matching criteria"})
+                }
 
     except ClientError as e:
         # Handle errors
@@ -105,3 +111,17 @@ def upload_images_to_s3(bucket_name, case_id, original_pdf_key, images):
         s3.upload_file(temp_image_path, bucket_name, destination_key)
         os.remove(temp_image_path)  # Eliminar archivo temporal
 
+
+def update_dynamodb_status(case_id, new_status):
+    """ Actualiza el campo 'status' de un registro en DynamoDB a 'new_status' """
+    try:
+        dynamodb.update_item(
+            TableName=TABLE_NAME,
+            Key={"case_id": {"S": case_id}},
+            UpdateExpression="SET #s = :new_status",
+            ExpressionAttributeNames={"#s": "status"},
+            ExpressionAttributeValues={":new_status": {"S": f"{new_status}"}}
+        )
+        print(f"Status actualizado a '{new_status}' para case_id: {case_id}")
+    except ClientError as e:
+        print(f"Error actualizando status en DynamoDB: {e.response['Error']['Message']}")
